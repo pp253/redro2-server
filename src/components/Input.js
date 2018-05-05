dc2b3fa328dd52ffb292e2d72b06fe0703fa6514
pp253 <pp.pp253@gmail.com>
2018-05-06 02:24:41 +0800

    new components

---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+this.context = {}
+this.actions = {}
+this.options = {
+  orderable: true,
+  assemblable: true,
+  importer: new Map(),
+  exporter: new Map(),
+  importGoods: new Map(),
+  exportGoods: new Map()
+}
