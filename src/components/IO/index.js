97f11a7074ba6f489d6ff7cc5fdfd1ed0bb9b991
pp253 <pp.pp253@gmail.com>
2018-07-11 10:29:45 +0800

    fix
    This reverts commit bab41373b0a460944c6d695d9b528a232b1975cd.

--- src/components/IO/index.js
+++ src/components/IO/index.js
@@ -225,15 +225,7 @@ export default class IO extends EventEmitter {
       })
       .then((store) => {
         let exportJournal = store.state.exportJournal.toObject()
-<<<<<<< HEAD
-<<<<<<< HEAD
-        let iji = exportJournal[exportJournal.legnth - 1]
-=======
         let iji = exportJournal[exportJournal.length - 1]
->>>>>>> parent of bab4137... build
-=======
-        let iji = exportJournal[exportJournal.length - 1]
->>>>>>> parent of bab4137... build
 
         if (iji.transportationStatus === TRANSPORTATION_STATUS.DELIVERING) {
           let gta = this.engine.gameTimeAdd(iji.gameTime, iji.transportationTime)
