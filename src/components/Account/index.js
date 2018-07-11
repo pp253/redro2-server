3b2c4b0d786a4d443bf98ce4a87839bc309e439b
pp253 <pp.pp253@gmail.com>
2018-07-11 10:38:32 +0800

    Revert "fix"
    This reverts commit a8dd41a3dba1758167f535c45d94a7e4d44affe6.

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -12,7 +12,6 @@ export default class Account extends EventEmitter {
     this.type = 'Account'
     this._loaded = false
     this._bankrupt = false
-    this.serial = 0
     this.setMaxListeners(10000)
   }
 
@@ -86,10 +85,10 @@ export default class Account extends EventEmitter {
       // check balance
       if (!('unbalance' in accountTransaction) || accountTransaction.unbalance === false) {
         let debitAmount = accountTransaction.debit ? accountTransaction.debit.reduce((acc, item) => {
-          return acc + parseFloat(item.amount)
+          return acc + item.amount
         }, 0) : 0
         let creditAmount = accountTransaction.credit ? accountTransaction.credit.reduce((acc, item) => {
-          return acc + parseFloat(item.amount)
+          return acc + item.amount
         }, 0) : 0
 
         if (debitAmount !== creditAmount) {
@@ -97,22 +96,15 @@ export default class Account extends EventEmitter {
         }
       }
 
-      if (!accountTransaction.time) {
-        accountTransaction.time = Date.now()
-      }
-      accountTransaction.serial = this.serial++
-      let serial = accountTransaction.serial
-
       this.store.dispatch('addTransaction', accountTransaction)
       .then(() => {
         let journal = this.getJournal()
-        let transaction = journal.find(item => item.serial === serial)
         this.emit(ACCOUNT_EVENTS.ACCOUNT_ADD, new AccountEvent({
           isBankrupt: this.isBankrupt(),
-          transaction: transaction,
+          transaction: journal[journal.length - 1],
           type: ACCOUNT_EVENTS.ACCOUNT_ADD,
           target: this,
-          gameTime: transaction.gameTime,
+          gameTime: accountTransaction.gameTime,
           nodeName: this.node.getName(),
           engineId: this.engine.getId()
         }))
@@ -120,7 +112,7 @@ export default class Account extends EventEmitter {
         if (options && options.noRepay === true) {
           return Promise.resolve()
         }
-        return this.repay(transaction)
+        return this.repay(accountTransaction)
       })
       .then(() => { resolve(this) })
       .catch(err => { reject(err) })
